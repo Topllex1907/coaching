eb1eb9f2bcbcc87b5a54110ff776666c05976b7d
Topllex1907 <95535700+Topllex1907@users.noreply.github.com>
2021-12-08 20:04:33 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { CounterCaner } from "./counterCaner.js";
+
+function App() {
+  return (
+    <div className="App">
+      <CounterCaner />
+    </div>
+  );
+}
+
+export default App;
